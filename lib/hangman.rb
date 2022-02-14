c294f1f02235370d1cb8cd2f179b31c179ad9b61
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-02-14 20:22:11 +0100

    Load words

---
+++ lib/hangman.rb
@@ -0,0 +1,11 @@
+# load word set
+lexicon = []
+
+words_filepath = File.join(File.dirname(__FILE__), '_data', 'google-10000-english-no-swears.txt')
+
+File.open(words_filepath, 'r') do |file|
+  until file.eof?
+    word = file.readline.strip
+    lexicon << word if word.length.between?(5, 12)
+  end
+end
