c911281be39c3be413ec5f04aa5bb749bc623bb7
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-02-15 15:04:43 +0100

    Implement a straight single round

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -9,3 +9,50 @@ File.open(words_filepath, 'r') do |file|
     lexicon << word if word.length.between?(5, 12)
   end
 end
+
+# choose word
+word = lexicon[rand(lexicon.length)]
+
+# mistake counter
+mistakes = 0
+
+MAX_MISTAKES = 5
+
+guesses = []
+
+hits = 0
+
+def print_word_and_guesses(word, guesses)
+  correct, incorrect = guesses.partition { |char| word.include?(char) }
+  puts word.gsub(/./) { |char| correct.include?(char) ? char : '_' } .split('').join(' ') + "\t" + incorrect.join(', ')
+end
+
+while mistakes < MAX_MISTAKES
+  puts "#{mistakes} / #{MAX_MISTAKES}"
+
+  print_word_and_guesses(word, guesses)
+
+  print 'Enter guess: '
+  
+  guess = gets.strip
+
+  guess.each_char do |char|
+    unless guesses.include?(char)
+      guesses << char
+
+      mistakes += 1 unless word.include?(char)
+      hits +=  word.count(char)
+    end
+  end
+
+  if hits == word.length
+    puts "Correct!"
+    print_word_and_guesses(word, guesses)
+    break
+  end
+end
+
+if mistakes == MAX_MISTAKES
+  puts "You lost. The word was:"
+  puts word.split('').join(' ')
+end
